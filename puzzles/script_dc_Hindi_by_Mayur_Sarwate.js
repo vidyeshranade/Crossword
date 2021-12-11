14dbfc8fe55e29296050e3845115a041721fe7bc
vidyeshranade <vidyesh.ranade@gmail.com>
2021-12-11 15:47:14 +0530

    updated clues for Hindi and Punjabi Puzzles

--- puzzles/script_dc_Hindi_by_Mayur_Sarwate.js
+++ puzzles/script_dc_Hindi_by_Mayur_Sarwate.js
@@ -7,7 +7,7 @@
 		// two entries: an across entry and a down entry
 		var puzzleData = [
 {
-	 clue: "झाबुआ district is famous for …-(3)",
+	 clue: "This district is famous for White Gold -(3)",
 	 answer: "झाबुआ",
 	 attempt: "", 
 	 position: 1,
@@ -16,7 +16,7 @@
 	 starty: 21
 },
 {
-	 clue: "सागर district is famous for …-(3)",
+	 clue: "This district is famous for University-(3)",
 	 answer: "सागर",
 	 attempt: "", 
 	 position: 2,
@@ -25,7 +25,7 @@
 	 starty: 20
 },
 {
-	 clue: "आगर district is famous for …-(3)",
+	 clue: "This district name starts with: Fire-(3)",
 	 answer: "आगर",
 	 attempt: "", 
 	 position: 3,
@@ -34,7 +34,7 @@
 	 starty: 21
 },
 {
-	 clue: "देवास district is famous for …-(3)",
+	 clue: "This district is famous for Devi Chamunda temple-(3)",
 	 answer: "देवास",
 	 attempt: "", 
 	 position: 4,
@@ -43,7 +43,7 @@
 	 starty: 23
 },
 {
-	 clue: "विदिशा district is famous for …-(3)",
+	 clue: "This district is famous for उदयगिरि की गुफाओं-(3)",
 	 answer: "विदिशा",
 	 attempt: "", 
 	 position: 5,
@@ -52,7 +52,7 @@
 	 starty: 28
 },
 {
-	 clue: "रतलाम district is famous for …-(4)",
+	 clue: "This district is famous for राजा महाराजा रतन सिंह-(4)",
 	 answer: "रतलाम",
 	 attempt: "", 
 	 position: 6,
@@ -61,7 +61,7 @@
 	 starty: 22
 },
 {
-	 clue: "सतना district is famous for …-(3)",
+	 clue: "This district is famous for चित्रकूट धाम और मैहर -(3)",
 	 answer: "सतना",
 	 attempt: "", 
 	 position: 7,
@@ -70,7 +70,7 @@
 	 starty: 23
 },
 {
-	 clue: "सीहोर district is famous for …-(3)",
+	 clue: "This district is famous for Bhuteshwar Temple-(3)",
 	 answer: "सीहोर",
 	 attempt: "", 
 	 position: 8,
@@ -79,7 +79,7 @@
 	 starty: 29
 },
 {
-	 clue: "This district is famous for SPM Paper Mills-(5)",
+	 clue: "This district is famous for Security Paper Mills (Note paper Printing press)-(5)",
 	 answer: "होशंगाबाद",
 	 attempt: "", 
 	 position: 9,
@@ -88,7 +88,7 @@
 	 starty: 30
 },
 {
-	 clue: "शाजापुर district is famous for …-(4)",
+	 clue: "This district is famous for Raj-Rajeshwari Templte built in 10th century-(4)",
 	 answer: "शाजापुर",
 	 attempt: "", 
 	 position: 10,
@@ -97,7 +97,7 @@
 	 starty: 28
 },
 {
-	 clue: "उज्जैन district is famous for …-(3)",
+	 clue: "This district is famous for Mahankamleshewar-(3)",
 	 answer: "उज्जैन",
 	 attempt: "", 
 	 position: 11,
@@ -106,7 +106,7 @@
 	 starty: 25
 },
 {
-	 clue: "उमरिया district is famous for …-(4)",
+	 clue: "This district is famous for Coal Mines-(4)",
 	 answer: "उमरिया",
 	 attempt: "", 
 	 position: 11,
@@ -115,7 +115,7 @@
 	 starty: 25
 },
 {
-	 clue: "दतिया district is famous for …-(3)",
+	 clue: "This district is famous for Jain Temple-(3)",
 	 answer: "दतिया",
 	 attempt: "", 
 	 position: 12,
@@ -124,7 +124,7 @@
 	 starty: 23
 },
 {
-	 clue: "नरसिंहपुर district is famous for …-(6)",
+	 clue: "This district name starts with: Man-Lion-Pur-(6)",
 	 answer: "नरसिंहपुर",
 	 attempt: "", 
 	 position: 13,
@@ -133,7 +133,7 @@
 	 starty: 27
 },
 {
-	 clue: "नगर district is famous for …-(3)",
+	 clue: "This district is City in English -(3)",
 	 answer: "नगर",
 	 attempt: "", 
 	 position: 13,
@@ -142,7 +142,7 @@
 	 starty: 27
 },
 {
-	 clue: "बालाघाट district is famous for …-(4)",
+	 clue: "This district is famous for मैंगनीज एवं तांबा (एशिया महाद्वीप का सबसे बड़ा फेरो मैंगनीज प्लांट भरवेली में लगाया गया )-(4)",
 	 answer: "बालाघाट",
 	 attempt: "", 
 	 position: 14,
@@ -151,7 +151,7 @@
 	 starty: 30
 },
 {
-	 clue: "दमोह district is famous for …-(3)",
+	 clue: "This district is famous for जटाशंकर मंदिर,  रानी दमयंती संग्रहालय-(3)",
 	 answer: "दमोह",
 	 attempt: "", 
 	 position: 15,
@@ -160,7 +160,7 @@
 	 starty: 30
 },
 {
-	 clue: "सिंगरौली district is famous for …-(4)",
+	 clue: "This district is famous for Capital of energy, Coal City-(4)",
 	 answer: "सिंगरौली",
 	 attempt: "", 
 	 position: 16,
@@ -169,7 +169,7 @@
 	 starty: 29
 },
 {
-	 clue: "कटनी district is famous for …-(3)",
+	 clue: "This district is famous for Limestone City-(3)",
 	 answer: "कटनी",
 	 attempt: "", 
 	 position: 17,
@@ -178,7 +178,7 @@
 	 starty: 33
 },
 {
-	 clue: "बुरहानपुर district is famous for …-(6)",
+	 clue: "This district is famous for Khandesh sultanate-(6)",
 	 answer: "बुरहानपुर",
 	 attempt: "", 
 	 position: 18,
@@ -187,7 +187,7 @@
 	 starty: 32
 },
 {
-	 clue: "अलीराजपुर district is famous for …-(6)",
+	 clue: "This district is famous for लकड़ी की नक्काशी-(6)",
 	 answer: "अलीराजपुर",
 	 attempt: "", 
 	 position: 19,
@@ -196,7 +196,7 @@
 	 starty: 36
 },
 {
-	 clue: "ग्वालियर district is famous for …-(4)",
+	 clue: "This district is famous for Tansen handicrafts-(4)",
 	 answer: "ग्वालियर",
 	 attempt: "", 
 	 position: 20,
@@ -205,7 +205,7 @@
 	 starty: 29
 },
 {
-	 clue: "अनूपपुर district is famous for …-(5)",
+	 clue: "This district is famous for hills and forests-(5)",
 	 answer: "अनूपपुर",
 	 attempt: "", 
 	 position: 21,
@@ -214,7 +214,7 @@
 	 starty: 36
 },
 {
-	 clue: "नर्मदापुरम district is famous for …-(6)",
+	 clue: "This district is famous for Sethani ghat-(6)",
 	 answer: "नर्मदापुरम",
 	 attempt: "", 
 	 position: 22,
@@ -223,7 +223,7 @@
 	 starty: 32
 },
 {
-	 clue: "मंदसौर district is famous for …-(4)",
+	 clue: "This district is famous for Pashupatinath (Parellel Temple in Nepal)-(4)",
 	 answer: "मंदसौर",
 	 attempt: "", 
 	 position: 23,
@@ -232,7 +232,7 @@
 	 starty: 40
 },
 {
-	 clue: "मंडला district is famous for …-(3)",
+	 clue: "This district is famous for Kanha National Park-(3)",
 	 answer: "मंडला",
 	 attempt: "", 
 	 position: 23,
@@ -241,7 +241,7 @@
 	 starty: 40
 },
 {
-	 clue: "हरदा district is famous for …-(3)",
+	 clue: "This district is famous for Nabhi Kund-(3)",
 	 answer: "हरदा",
 	 attempt: "", 
 	 position: 24,
@@ -250,7 +250,7 @@
 	 starty: 34
 },
 {
-	 clue: "राजगढ़ district is famous for …-(4)",
+	 clue: "This district is famous for Shrinath temple (river Nevaj)-(4)",
 	 answer: "राजगढ़",
 	 attempt: "", 
 	 position: 25,
@@ -259,7 +259,7 @@
 	 starty: 36
 },
 {
-	 clue: "पन्ना district is famous for …-(2)",
+	 clue: "This district is famous for Diamond Mines-(2)",
 	 answer: "पन्ना",
 	 attempt: "", 
 	 position: 26,
--- puzzles/script_dc_Punjabi_by_Ramanjeet_Saini.js
+++ puzzles/script_dc_Punjabi_by_Ramanjeet_Saini.js
@@ -7,7 +7,7 @@
 		// two entries: an across entry and a down entry
 		var puzzleData = [
 {
-	 clue: "ਅੰਮ੍ਰਿਤਸਰ district is famous for …-(6)",
+	 clue: "This district is famous for The GOLDEN TEMPLE/ ਗੋਲਡਨ ਟੈਂਪਲ-(6)",
 	 answer: "ਅੰਮ੍ਰਿਤਸਰ",
 	 attempt: "", 
 	 position: 1,
@@ -16,7 +16,7 @@
 	 starty: 25
 },
 {
-	 clue: "ਸ਼ਹੀਦਭਗਤਸਿੰਘਨਗਰ district is famous for …-(11)",
+	 clue: "This district is famous for FREEDOM FIGHTER BHAGAT SINGH's HOME/ ਆਜ਼ਾਦੀ ਘੁਲਾਟੀਏ ਭਗਤ ਸਿੰਘ ਦਾ ਘਰ-(11)",
 	 answer: "ਸ਼ਹੀਦਭਗਤਸਿੰਘਨਗਰ",
 	 attempt: "", 
 	 position: 2,
@@ -25,7 +25,7 @@
 	 starty: 24
 },
 {
-	 clue: "ਫਿਰੋਜ਼ਪੁਰ district is famous for …-(5)",
+	 clue: "This district is famous for LAND OF MARTYRS/ ਸ਼ਹੀਦਾਂ ਦੀ ਧਰਤੀ -(5)",
 	 answer: "ਫਿਰੋਜ਼ਪੁਰ",
 	 attempt: "", 
 	 position: 3,
@@ -34,7 +34,7 @@
 	 starty: 30
 },
 {
-	 clue: "ਫ਼ਤਹਿਗੜ੍ਹਸਾਹਿਬ district is famous for …-(9)",
+	 clue: "This district is famous for ਗੁਰੂ ਗੋਬਿੰਦ ਸਿੰਘ ਜੀ ਦੇ ਛੋਟੇ ਸਾਹਿਬਜ਼ਾਦਿਆਂ ਦੀ ਮਹਾਨ ਸ਼ਹਾਦਤ ਨੂੰ ਸਮਰਪਿਤ ਗੁਰਦੁਆਰਾ ਸ੍ਰੀ ਫਤਹਿਗੜ੍ਹ ਸਾਹਿਬ-(9)",
 	 answer: "ਫ਼ਤਹਿਗੜ੍ਹਸਾਹਿਬ",
 	 attempt: "", 
 	 position: 4,
@@ -43,7 +43,7 @@
 	 starty: 28
 },
 {
-	 clue: "ਅਜੀਤਗੜ੍ਹ district is famous for …-(6)",
+	 clue: "This district is famous for One of the best cricket stadiums in India/ ਭਾਰਤ ਦੇ ਸਭ ਤੋਂ ਵਧੀਆ ਕ੍ਰਿਕਟ ਸਟੇਡੀਅਮਾਂ ਵਿੱਚੋਂ ਇੱਕ-(6)",
 	 answer: "ਅਜੀਤਗੜ੍ਹ",
 	 attempt: "", 
 	 position: 5,
@@ -52,7 +52,7 @@
 	 starty: 23
 },
 {
-	 clue: "ਹੁਸ਼ਿਆਰਪੁਰ district is famous for …-(6)",
+	 clue: "This district is famous for Wooden and Ivory Handicraft/ ਲੱਕੜ ਅਤੇ ਹਾਥੀ ਦੰਦ ਦੀ ਕਲਾਕਾਰੀ -(6)",
 	 answer: "ਹੁਸ਼ਿਆਰਪੁਰ",
 	 attempt: "", 
 	 position: 6,
@@ -61,7 +61,7 @@
 	 starty: 34
 },
 {
-	 clue: "ਗੁਰਦਾਸਪੁਰ district is famous for …-(6)",
+	 clue: "This district is famous for Birth city of legendary Actor Dev Anand/ ਮਸ਼ਹੂਰ ਅਦਾਕਾਰ ਦੇਵ ਆਨੰਦ ਦਾ ਜਨਮ ਸ਼ਹਿਰ-(6)",
 	 answer: "ਗੁਰਦਾਸਪੁਰ",
 	 attempt: "", 
 	 position: 7,
@@ -70,7 +70,7 @@
 	 starty: 33
 },
 {
-	 clue: "ਮੁਕਤਸਰ district is famous for …-(5)",
+	 clue: "This district is famous for Beaded Footwear/ ਮਣਕੇ ਵਾਲੀ ਜੁੱਤੀ-(5)",
 	 answer: "ਮੁਕਤਸਰ",
 	 attempt: "", 
 	 position: 8,
@@ -88,7 +88,7 @@
 	 starty: 32
 },
 {
-	 clue: "ਬਰਨਾਲਾ district is famous for …-(4)",
+	 clue: "This district is famous for Cotton Fields/ ਕਪਾਹ ਦੇ ਖੇਤ-(3)-(4)",
 	 answer: "ਬਰਨਾਲਾ",
 	 attempt: "", 
 	 position: 10,
@@ -97,7 +97,7 @@
 	 starty: 28
 },
 {
-	 clue: "ਪਟਿਆਲਾ district is famous for …-(4)",
+	 clue: "This district is famous for Traditional Royal Turban/ ਰਵਾਇਤੀ ਸ਼ਾਹੀ ਪੱਗ-(4)",
 	 answer: "ਪਟਿਆਲਾ",
 	 attempt: "", 
 	 position: 11,
@@ -106,7 +106,7 @@
 	 starty: 31
 },
 {
-	 clue: "ਤਰਨਤਾਰਨ district is famous for …-(6)",
+	 clue: "This district is famous for Largest Lake/ Sarovar (Holy Tank) of all the Gurudwaras/ ਸਾਰੇ ਗੁਰਦੁਆਰਿਆਂ ਵਿੱਚੋਂ ਸਭ ਤੋਂ ਵੱਡਾ ਸਰੋਵਰ (ਪਵਿੱਤਰ ਸਰੋਵਰ)-(6)",
 	 answer: "ਤਰਨਤਾਰਨ",
 	 attempt: "", 
 	 position: 12,
@@ -115,7 +115,7 @@
 	 starty: 38
 },
 {
-	 clue: "ਰੂਪਨਗਰ district is famous for …-(5)",
+	 clue: "This district is famous for First Indus Valley Excavation Site/ ਪਹਿਲੀ ਸਿੰਧੂ ਘਾਟੀ ਖੁਦਾਈ ਸਾਈਟ-(5)",
 	 answer: "ਰੂਪਨਗਰ",
 	 attempt: "", 
 	 position: 13,
@@ -124,7 +124,7 @@
 	 starty: 36
 },
 {
-	 clue: "ਪਠਾਨਕੋਟ district is famous for …-(5)",
+	 clue: "This district is famous for Shawl weaving industry/ ਸ਼ਾਲ ਬੁਣਾਈ ਉਦਯੋਗ-(5)",
 	 answer: "ਪਠਾਨਕੋਟ",
 	 attempt: "", 
 	 position: 14,
@@ -133,7 +133,7 @@
 	 starty: 36
 },
 {
-	 clue: "ਸੰਗਰੂਰ district is famous for …-(4)",
+	 clue: "This district is famous for City Of Gurudwaras/ ਗੁਰਦੁਆਰਿਆਂ ਦਾ ਸ਼ਹਿਰ-(4)",
 	 answer: "ਸੰਗਰੂਰ",
 	 attempt: "", 
 	 position: 15,
@@ -142,7 +142,7 @@
 	 starty: 33
 },
 {
-	 clue: "ਫਰੀਦਕੋਟ district is famous for …-(5)",
+	 clue: "This district का निर्माण 1972 में बठिंडा और फिरोज़पुर जिलों से किया गया।  इस शहर का नाम सूफी संत बाबा शेख़ फ़रीदुद्दीन गंजशाकर के नाम पर पड़ा-(5)",
 	 answer: "ਫਰੀਦਕੋਟ",
 	 attempt: "", 
 	 position: 16,
